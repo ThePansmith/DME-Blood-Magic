4596b31ee541463b755fa6e4f7275a6207615b9a
bruberu <80226372+bruberu@users.noreply.github.com>
2022-12-28 13:28:50 -0600

    The basics

---
+++ src/main/java/deepmoblearningbm/DeepMobLearningBM.java
@@ -0,0 +1,9 @@
+package deepmoblearningbm;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ModConstants.MODID, version = ModConstants.VERSION, dependencies =
+        "required-after:deepmoblearning;required-after:bloodmagic;after:jei;after:twilightforest")
+public class DeepMobLearningBM {
+
+}
---
+++ src/main/java/deepmoblearningbm/ModConstants.java
@@ -0,0 +1,10 @@
+package deepmoblearningbm;
+
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+
+public class ModConstants {
+    public static final String MODID = "deepmoblearningbm";
+    public static final String VERSION = "@VERSION@";
+    public static final boolean MOD_BM_LOADED = Loader.isModLoaded("bloodmagic");
+}
