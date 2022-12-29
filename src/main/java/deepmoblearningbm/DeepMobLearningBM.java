ac44522453f1f1cbafc5b3a03a7db73f73d9634c
MaxistheSpy <maxisthespy@gmail.com>
2022-12-28 19:02:34 -0800

    First stages of implementing the Digital agonizer. Created Class for BlockDigitalAgonizer Created Skeleton of ContainerDigitalAgonizer Created digital_agonizer.json this is a temporary texture and should be revisited Added a logger to DeepMobLearningBM and a call to getModLog in the preInit stage (im not sure what this does) Refactored ModConstants to include ModInfo class for neatness Changes TileEntityDigitalAgonizer: -updateCraftingState now always returns IDLE instead of null -> this is temporary to prevent NPE's until proper implementation of the method.
    -Fully Implemented getContainer
    
    Implemented full skeleton of DMLBM Registry inclduing:
    -Register blocks
    -Register items: includes registering itemblocks
    -Register Entities: currently does nothing
    -Register Recipies: Im pretty sure this also does nothing as of now
    -Added loggers to each indicating when they start
    -Registered DigitalAgonizer
    
    Changes to DMLBMConfig include:
    -Config for maximum RF capacity of agonizer currently defaults to MAXINT
    -Config for the agonizers maximum input RF/t also defaults to MAXINT
    Both of these were required for the Digital Agonizer constructor and i figured I might aswell make a config for it.
    
    By the end of this the mob agonizer block exists and can be placed it also seems to not crash when interacted with. no gui or rightclick functionality yet.

--- src/main/java/deepmoblearningbm/DeepMobLearningBM.java
+++ src/main/java/deepmoblearningbm/DeepMobLearningBM.java
@@ -2,18 +2,27 @@ package deepmoblearningbm;
 
 import deepmoblearningbm.common.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ModConstants.MODID, version = ModConstants.VERSION, dependencies =
+@Mod(modid = ModConstants.ModInfo.MODID, version = ModConstants.ModInfo.VERSION, dependencies =
         "required-after:deepmoblearning;required-after:bloodmagic;after:jei;after:twilightforest")
 public class DeepMobLearningBM {
-    @Mod.Instance(ModConstants.MODID)
+    @Mod.Instance(ModConstants.ModInfo.MODID)
     public static DeepMobLearningBM instance;
 
+    public static Logger logger;
+
     @SidedProxy(
-            clientSide="deepmoblearningbm.client.ClientProxy",
-            serverSide="deepmoblearningbm.common.CommonProxy"
+            clientSide = "deepmoblearningbm.client.ClientProxy",
+            serverSide = "deepmoblearningbm.common.CommonProxy"
     )
     public static CommonProxy proxy;
 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
 }
--- src/main/java/deepmoblearningbm/common/tiles/TileEntityDigitalAgonizer.java
+++ src/main/java/deepmoblearningbm/common/tiles/TileEntityDigitalAgonizer.java
@@ -1,11 +1,13 @@
 package deepmoblearningbm.common.tiles;
 
-import deepmoblearningbm.DMLBMConfig;
+import deepmoblearningbm.common.Inventory.ContainerDigitalAgonizer;
 import mustapelto.deepmoblearning.common.inventory.ContainerTileEntity;
 import mustapelto.deepmoblearning.common.inventory.ItemHandlerDataModel;
 import mustapelto.deepmoblearning.common.tiles.CraftingState;
 import mustapelto.deepmoblearning.common.tiles.TileEntityMachine;
 import net.minecraft.entity.player.InventoryPlayer;
+
+import static deepmoblearningbm.DMLBMConfig.MACHINE_SETTINGS;
 
 public class TileEntityDigitalAgonizer extends TileEntityMachine {
     private final ItemHandlerDataModel inputDataModel = new ItemHandlerDataModel() {
@@ -16,6 +18,10 @@ public class TileEntityDigitalAgonizer extends TileEntityMachine {
     };
 
     private void onDataModelChanged() {
+    }
+
+    public TileEntityDigitalAgonizer() {
+        super(MACHINE_SETTINGS.DIGITAL_AGONIZER_RF_CAPACITY, MACHINE_SETTINGS.DIGITAL_AGONIZER_RF_INPUTMAX);
     }
 
     public TileEntityDigitalAgonizer(int energyCapacity, int energyMaxReceive) {
@@ -34,16 +40,16 @@ public class TileEntityDigitalAgonizer extends TileEntityMachine {
 
     @Override
     public int getCraftingEnergyCost() {
-        return DMLBMConfig.MACHINE_SETTINGS.DIGITAL_AGONIZER_RF_COST;
+        return MACHINE_SETTINGS.DIGITAL_AGONIZER_RF_COST;
     }
 
     @Override
     protected CraftingState updateCraftingState() {
-        return null;
+        return CraftingState.IDLE;
     }
 
     @Override
     public ContainerTileEntity getContainer(InventoryPlayer inventoryPlayer) {
-        return null;
+        return new ContainerDigitalAgonizer(this,inventoryPlayer);
     }
 }
